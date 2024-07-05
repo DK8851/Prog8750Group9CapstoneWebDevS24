6cc5a619cdacd31427c06530efb7185380306a40
Darshan <131203570+DK8851@users.noreply.github.com>
2024-07-04 18:39:14 -0600

    domain name added for third party images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // reactStrictMode: true
+    images: {
+        remotePatterns: [
+            {
+                hostname: "placekitten.com"
+            }
+        ]
+    }
 };
 
 export default nextConfig;
